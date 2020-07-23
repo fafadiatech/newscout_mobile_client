28fed69b0392a6b33f016515f704006e14f91eaf
Sidharth Shah <sidharth@fafadiatech.com>
2020-07-23 17:14:09 +0530

    ref #1: refactored ArticleDetailView screen

--- src/NewScout/screens/ArticleDetailsScreen.js
+++ src/NewScout/screens/ArticleDetailsScreen.js
@@ -14,6 +14,8 @@ import {TouchableOpacity} from 'react-native-gesture-handler';
 import styles from '../styles/Base';
 import * as Colors from '../styles/Colors';
 
+import componentStyles from '../styles/ArticleDetailsScreen';
+
 class ArticleDetailsScreen extends React.Component {
     constructor(props) {
       super(props);
@@ -54,21 +56,11 @@ class ArticleDetailsScreen extends React.Component {
                 <SafeAreaView style={styles.flexible}>
                 <View style={styles.slide1}>
                   <Image
-                        style={{
-                          flex: 1,
-                          backgroundColor: 'yellow',
-                          marginLeft: 10,
-                          marginRight: 5,
-                          marginTop: 15,
-                          marginBottom: 10,
-                          width: 355,
-                          height: 180,
-                          borderRadius: 10,
-                        }}
+                        style={componentStyles.imageStyle}
                         source={{uri: 'https://picsum.photos/400/400'}}
                   />
                   <View style={{flex: 1.5}}>
-                    <Text style={{flex: 0.60, marginLeft: 10, fontSize: 20, fontWeight: 'bold'}}>
+                    <Text style={componentStyles.titleStyle}>
                       Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.
                     </Text>
                     <View style={{flex: 0.25, flexDirection: 'row'}}>
@@ -76,98 +68,52 @@ class ArticleDetailsScreen extends React.Component {
                         within 5 hours
                       </Text>
                       <Text
-                          style={{
-                            flex: 1,
-                            color: Colors.basePrimaryColor,
-                            fontSize: 13,
-                            fontWeight: 'bold',
-                          }}>
+                          style={styles.source}>
                         financialexpress.com
                       </Text>
                     </View>
                     <Text
-                      style={{
-                        flex: 3,
-                        marginLeft: 10,
-                        marginRight: 10,
-                        fontSize: 17,
-                      }}>
+                      style={componentStyles.blurbStyle}>
                       Anim incididunt esse eiusmod duis ullamco. Exercitation exercitation amet exercitation duis Lorem velit eu. Culpa consequat anim minim est elit. Tempor dolore magna consequat consequat sunt consectetur amet aliquip consectetur aliqua adipisicing culpa. Exercitation in velit dolore quis irure irure in aute dolore est velit quis ullamco fugiat. Labore ut elit adipisicing ea. Ut ex ex anim nulla do duis eiusmod anim ex.
                       Exercitation in velit dolore quis irure irure in aute dolore est velit quis ullamco fugiat. Labore ut elit adipisicing ea. Ut ex ex anim nulla do duis eiusmod anim ex.
                   </Text>
                   <TouchableOpacity>
                     <View
-                      style={{
-                        zIndex: 1,
-                        backgroundColor: Colors.basePrimaryColor,
-                        alignItems: 'center',
-                        borderRadius: 10,
-                        height: 45,
-                        marginTop: 5,
-                        marginLeft: 5,
-                        marginRight: 5,
-                        marginBottom: 10,
-                      }}>
+                      style={componentStyles.readMoreViewStyle}>
                       <Text
-                        style={{
-                          color: 'white',
-                          fontSize: 18,
-                          fontWeight: 'bold',
-                          marginTop: 12,
-                          marginBottom: 5,
-                        }}>
+                        style={componentStyles.readMoreButtonStyle}>
                         Read More
                       </Text>
                     </View>
                   </TouchableOpacity>
                   </View>
-                  <View style={{flex: 0.20, flexDirection: 'row', backgroundColor: Colors.basePrimaryColor, justifyContent: 'space-around'}}>
+                  <View style={componentStyles.bottomBarStyle}>
                       <Icon
                         name={'random'}
                         size={21}
-                        color={'white'}
-                        style={{
-                          flex: 0.1,
-                          alignSelf: 'center',
-                          marginLeft: 5,
-                          marginRight: 10,
-                        }}
+                        color={Colors.iconColor}
+                        style={componentStyles.bottomBarIconStyle}
                       />
                       <Icon
                         name={'comment'}
                         size={21}
-                        color={'white'}
-                        style={{
-                          flex: 0.1,
-                          alignSelf: 'center',
-                          marginLeft: 5,
-                          marginRight: 10,
-                        }}
+                        color={Colors.iconColor}
+                        style={componentStyles.bottomBarIconStyle}
                       />
-                      <Text style={{color: 'white', fontWeight: 'bold', fontSize: 20, marginTop: 10}}>
+                      <Text style={componentStyles.moreStoriesStyle}>
                           More Stories
                       </Text>
                       <Icon
                         name={'arrow-up'}
                         size={21}
-                        color={'white'}
-                        style={{
-                          flex: 0.1,
-                          alignSelf: 'center',
-                          marginLeft: 5,
-                          marginRight: 10,
-                        }}
+                        color={Colors.iconColor}
+                        style={componentStyles.bottomBarIconStyle}
                       />
                       <Icon
                         name={'share-alt'}
                         size={21}
-                        color={'white'}
-                        style={{
-                          flex: 0.1,
-                          alignSelf: 'center',
-                          marginLeft: 5,
-                          marginRight: 10,
-                        }}
+                        color={Colors.iconColor}
+                        style={componentStyles.bottomBarIconStyle}
                       />
                   </View>
                 </View>
---
+++ src/NewScout/styles/ArticleDetailsScreen.js
@@ -0,0 +1,66 @@
+import {StyleSheet} from 'react-native';
+import * as Colors from './Colors';
+
+const componentStyles = StyleSheet.create({
+    imageStyle: {
+        flex: 1,
+        backgroundColor: Colors.baseBackgroundSecondaryColor,
+        marginLeft: 10,
+        marginRight: 5,
+        marginTop: 15,
+        marginBottom: 10,
+        width: 355,
+        height: 180,
+        borderRadius: 10,
+    },
+    titleStyle: {
+        flex: 0.60, 
+        marginLeft: 10, 
+        fontSize: 20, 
+        fontWeight: 'bold'
+    },
+    blurbStyle: {
+        flex: 3,
+        marginLeft: 10,
+        marginRight: 10,
+        fontSize: 17,
+    },
+    readMoreViewStyle: {
+        zIndex: 1,
+        backgroundColor: Colors.basePrimaryColor,
+        alignItems: 'center',
+        borderRadius: 10,
+        height: 45,
+        marginTop: 5,
+        marginLeft: 5,
+        marginRight: 5,
+        marginBottom: 10,
+    },
+    readMoreButtonStyle: {
+        color: Colors.iconColor,
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: 12,
+        marginBottom: 5,
+    },
+    bottomBarStyle: {
+        flex: 0.20,
+        flexDirection: 'row',
+        backgroundColor: Colors.basePrimaryColor,
+        justifyContent: 'space-around'
+    },
+    bottomBarIconStyle: {
+        flex: 0.1,
+        alignSelf: 'center',
+        marginLeft: 5,
+        marginRight: 10,
+    },
+    moreStoriesStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginTop: 10
+    }
+});
+
+export default componentStyles;
