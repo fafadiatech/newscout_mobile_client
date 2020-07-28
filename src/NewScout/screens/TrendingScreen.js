e050ba68b04295a82098d9c776aad325ad6b1a24
Gaurav <gaurav.mehta@fafadiatech.com>
2020-07-28 16:43:05 +0530

    Added pull to refresh in trending screen.

--- src/NewScout/screens/TrendingScreen.js
+++ src/NewScout/screens/TrendingScreen.js
@@ -1,74 +1,106 @@
 import * as React from 'react';
-import {View, Text, ScrollView, FlatList, NativeModules, Platform} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  FlatList,
+  NativeModules,
+  Platform,
+  RefreshControl,
+} from 'react-native';
 
 import styles from '../styles/Base';
 
 import FeaturedItem from '../components/FeaturedItem';
-
+import * as Colors from '../styles/Colors';
 const {PlatformConstants} = NativeModules;
 const deviceType = PlatformConstants.interfaceIdiom;
 
 class TrendingScreen extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        dataSource: {},
-        cardColumns: 0,
-        screenOrientation: "",
-      };
-    }
-    callAPI = () => {
-      return fetch('http://www.newscout.in/api/v1/trending/?domain=newscout&format=json')
-        .then(response => response.json())
-        .then(json => {
-          this.setState({
-            featuredArticles: json.body.results,
-          });
-        })
-        .catch(error => {
-          console.error(error);
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: {},
+      cardColumns: 0,
+      screenOrientation: '',
+      refreshing: false,
     };
-  
-    componentDidMount() {
-      this.callAPI();
-    }
-
-    render() {
-
-      var cardColumns = 0;
-      if (Platform.isPad == true){
-          cardColumns = 3;
-      }else{
-          cardColumns = 1;
-      }
-
-      return (
-        <View style={styles.flexible}>
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-            style={styles.flexible}>
-            <Text style={styles.screenHeading}>
-              Trending
-            </Text>
-            <FlatList
-              numColumns={cardColumns}
-              style={styles.flexible}
-              data={this.state.featuredArticles}
-              renderItem={({item}) => (
-                <FeaturedItem 
-                  coverImage={item.articles[0].cover_image}
-                  title={item.articles[0].title}
-                  source={item.articles[0].source}
-                  ts={item.articles[0].published_on}
-                />
-              )}
-              keyExtractor={(item, index) => index.toString() + this.state.cardColumns.toString()}
-            />
-          </ScrollView>
-        </View>
-      );
-    }
   }
+  callAPI = () => {
+    return fetch(
+      'http://www.newscout.in/api/v1/trending/?domain=newscout&format=json',
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          featuredArticles: json.body.results,
+          refreshing: false,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        this.setState({
+          refreshing: false,
+        });
+      });
+  };
+
+  handleRefresh = () => {
+    this.setState(
+      {
+        refreshing: true,
+      },
+      () => {
+        this.callAPI();
+      },
+    );
+  };
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    var cardColumns = 0;
+    if (Platform.isPad == true) {
+      cardColumns = 3;
+    } else {
+      cardColumns = 1;
+    }
+
+    return (
+      <View style={styles.flexible}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl
+              colors={[Colors.basePrimaryColor]}
+              refreshing={this.state.refreshing}
+              onRefresh={this.handleRefresh}
+            />
+          }
+          style={styles.flexible}>
+          <Text style={styles.screenHeading}>Trending</Text>
+          <FlatList
+            numColumns={cardColumns}
+            style={styles.flexible}
+            data={this.state.featuredArticles}
+            renderItem={({item}) => (
+              <FeaturedItem
+                coverImage={item.articles[0].cover_image}
+                title={item.articles[0].title}
+                source={item.articles[0].source}
+                ts={item.articles[0].published_on}
+              />
+            )}
+            keyExtractor={(item, index) =>
+              index.toString() + this.state.cardColumns.toString()
+            }
+          />
+        </ScrollView>
+      </View>
+    );
+  }
+}
 
 export default TrendingScreen;
