7a81c30b16346721c535e7516bb5dfca68035bc0
Gaurav <gaurav.mehta@fafadiatech.com>
2020-07-30 18:45:10 +0530

    Added loading indicator.

--- src/NewScout/screens/ArticleDetailsScreen.js
+++ src/NewScout/screens/ArticleDetailsScreen.js
@@ -4,19 +4,24 @@ import {
   View,
   SafeAreaView,
   Image,
-  Platform
+  Platform,
+  ActivityIndicator,
 } from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { WebView } from 'react-native-webview';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {WebView} from 'react-native-webview';
 
 import Swiper from 'react-native-swiper';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
-import Modal, { SlideAnimation, ModalContent, ModalTitle } from 'react-native-modals';
+import Modal, {
+  SlideAnimation,
+  ModalContent,
+  ModalTitle,
+} from 'react-native-modals';
 
 import styles from '../styles/Base';
 import * as Colors from '../styles/Colors';
@@ -25,9 +30,9 @@ import componentStyles from '../styles/ArticleDetailsScreen';
 import MobileSuggestionsList from '../components/MobileSuggestionsList';
 import TabletSuggestionsList from '../components/TabletSuggestionsList';
 
-function SourceScreen({ route, navigation }) {
-  const { url } = route.params;
-  return <WebView source={{ uri: url }} />;
+function SourceScreen({route, navigation}) {
+  const {url} = route.params;
+  return <WebView source={{uri: url}} />;
 }
 
 class SuggestionsScreen extends React.Component {
@@ -35,7 +40,7 @@ class SuggestionsScreen extends React.Component {
     if (Platform.isPad == true) {
       this.setState({numOfRows: 8});
     }
-  }
+  };
 
   constructor(props) {
     super(props);
@@ -46,45 +51,54 @@ class SuggestionsScreen extends React.Component {
       data: [
         {
           coverImage: 'https://picsum.photos/200/200',
-          title: 'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
-          source: 'financialexpress.com'
+          title:
+            'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
+          source: 'financialexpress.com',
         },
         {
           coverImage: 'https://picsum.photos/200/200',
-          title: 'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
-          source: 'financialexpress.com'
+          title:
+            'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
+          source: 'financialexpress.com',
         },
         {
           coverImage: 'https://picsum.photos/200/200',
-          title: 'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
-          source: 'financialexpress.com'
+          title:
+            'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
+          source: 'financialexpress.com',
         },
         {
           coverImage: 'https://picsum.photos/200/200',
-          title: 'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
-          source: 'financialexpress.com'
+          title:
+            'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
+          source: 'financialexpress.com',
         },
         {
           coverImage: 'https://picsum.photos/200/200',
-          title: 'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
-          source: 'financialexpress.com'
+          title:
+            'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
+          source: 'financialexpress.com',
         },
         {
           coverImage: 'https://picsum.photos/200/200',
-          title: 'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
-          source: 'financialexpress.com'
+          title:
+            'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
+          source: 'financialexpress.com',
         },
         {
           coverImage: 'https://picsum.photos/200/200',
-          title: 'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
-          source: 'financialexpress.com'
+          title:
+            'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
+          source: 'financialexpress.com',
         },
         {
           coverImage: 'https://picsum.photos/200/200',
-          title: 'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
-          source: 'financialexpress.com'
+          title:
+            'Occaecat occaecat ullamco velit velit id dolor cupidatat pariatur deserunt fugiat.',
+          source: 'financialexpress.com',
         },
       ],
+      loading: false,
     };
     this.checkiPad();
   }
@@ -93,68 +107,54 @@ class SuggestionsScreen extends React.Component {
     this.checkiPad();
   };
 
-  render(){
-    if(this.state.numOfRows === 4){
+  render() {
+    if (this.state.numOfRows === 4) {
       return (
-        <View
-          onLayout={event => this.adaptToOrientationChange(event)}
-        >
+        <View onLayout={(event) => this.adaptToOrientationChange(event)}>
           <Modal.BottomModal
-            height={0.80}
+            height={0.8}
             width={1}
             visible={this.state.visible}
-            modalAnimation={new SlideAnimation({
-              slideFrom: 'bottom',
-            })}
-            modalTitle={
-              <ModalTitle
-                title="More Suggestions"
-                hasTitleBar
-              />
+            modalAnimation={
+              new SlideAnimation({
+                slideFrom: 'bottom',
+              })
             }
+            modalTitle={<ModalTitle title="More Suggestions" hasTitleBar />}
             onTouchOutside={() => {
-              this.setState({ visible: false });
+              this.setState({visible: false});
               this.props.navigation.goBack();
-            }}
-          >
+            }}>
             <ModalContent
               style={{
                 flex: 1,
-              }}
-            >
+              }}>
               <MobileSuggestionsList data={this.state.data} />
             </ModalContent>
           </Modal.BottomModal>
         </View>
       );
-    }else{
+    } else {
       return (
-        <View
-          onLayout={event => this.adaptToOrientationChange(event)}
-        >
+        <View onLayout={(event) => this.adaptToOrientationChange(event)}>
           <Modal.BottomModal
-            height={0.80}
+            height={0.8}
             width={1}
             visible={this.state.visible}
-            modalAnimation={new SlideAnimation({
-              slideFrom: 'bottom',
-            })}
-            modalTitle={
-              <ModalTitle
-                title="More Suggestions"
-                hasTitleBar
-              />
+            modalAnimation={
+              new SlideAnimation({
+                slideFrom: 'bottom',
+              })
             }
+            modalTitle={<ModalTitle title="More Suggestions" hasTitleBar />}
             onTouchOutside={() => {
-              this.setState({ visible: false });
+              this.setState({visible: false});
               this.props.navigation.goBack();
-            }}
-          >
+            }}>
             <ModalContent
               style={{
                 flex: 1,
-              }}
-            >
+              }}>
               <TabletSuggestionsList data={this.state.data} />
             </ModalContent>
           </Modal.BottomModal>
@@ -165,62 +165,82 @@ class SuggestionsScreen extends React.Component {
 }
 
 class ArticleDetailsView extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        data: ['Test 1', 'Test 2', 'Test 3'],
-        showSuggestions: false,
-      };
-      this.callAPI(this.props.route.params.articleSlug);
-    }
-  
-    callAPI = (articleSlug) => {
-      return fetch(`http://www.newscout.in/api/v1/articles/${articleSlug}/?domain=newscout&format=json`)
-        .then(response => response.json())
-        .then(json => {
-          this.setState({
-            data: [json.body.article],
-          });
-        })
-        .catch(error => {
-          console.error(error);
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [], //'Test 1', 'Test 2', 'Test 3'
+      showSuggestions: false,
+      loading: false,
     };
+    this.callAPI(this.props.route.params.articleSlug);
+  }
 
-    loadItems() {
-      this.setState({
-        data: [
-          'Test 1',
-          'Test 2',
-          'Test 3',
-          'Test 4',
-          'Test 5',
-          'Test 6',
-          'Test 7',
-        ],
+  callAPI = (articleSlug) => {
+    this.setState({
+      loading: true,
+    });
+    return fetch(
+      `http://www.newscout.in/api/v1/articles/${articleSlug}/?domain=newscout&format=json`,
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          data: [json.body.article],
+          loading: false,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        this.setState({
+          loading: false,
+        });
       });
-    }
+  };
 
-    render() {
-      return (
-        <Swiper
-          loop={false}
-          style={styles.wrapper}
-          horizontal={false}
-          showsPagination={false}
-          onIndexChanged={index => {
-            if (index === 1) {
-              this.loadItems();
-            }
-          }}>
-          {this.state.data.map &&
-            this.state.data.map((item, i) => {
-              return (
-                <SafeAreaView style={styles.flexible} key={i}>
+  loadItems() {
+    this.setState({
+      data: [
+        'Test 1',
+        'Test 2',
+        'Test 3',
+        'Test 4',
+        'Test 5',
+        'Test 6',
+        'Test 7',
+      ],
+    });
+  }
+
+  render() {
+    return (
+      <Swiper
+        loop={false}
+        style={styles.wrapper}
+        horizontal={false}
+        showsPagination={false}
+        onIndexChanged={(index) => {
+          if (index === 1) {
+            this.loadItems();
+          }
+        }}>
+        {this.state.data.length == 0 && (
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <ActivityIndicator
+              animating
+              size="large"
+              color={Colors.basePrimaryColor}
+            />
+          </View>
+        )}
+        {this.state.data.map &&
+          this.state.data.map((item, i) => {
+            return (
+              <SafeAreaView style={styles.flexible} key={i}>
                 <View style={styles.slide1}>
                   <Image
-                        style={componentStyles.imageStyle}
-                        source={{uri: item.cover_image}}
+                    style={componentStyles.imageStyle}
+                    source={{uri: item.cover_image}}
                   />
                   <View style={{flex: 1.5}}>
                     <Text numberOfLines={2} style={componentStyles.titleStyle}>
@@ -230,74 +250,80 @@ class ArticleDetailsView extends React.Component {
                       <Text style={{flex: 1, marginLeft: 10, fontSize: 13}}>
                         within 5 hours
                       </Text>
-                      <Text
-                          style={styles.source}>
-                        {item.source}
-                      </Text>
+                      <Text style={styles.source}>{item.source}</Text>
                     </View>
                     <Text
                       numberOfLines={10}
                       ellipsizeMode={'tail'}
                       style={componentStyles.blurbStyle}>
                       {item.blurb}
-                  </Text>
-                  <TouchableOpacity onPress={() => this.props.navigation.navigate('Source', {url: item.source_url})}>
-                    <View
-                      style={componentStyles.readMoreViewStyle}>
-                      <Text
-                        style={componentStyles.readMoreButtonStyle}>
-                        Read More
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
+                    </Text>
+                    <TouchableOpacity
+                      onPress={() =>
+                        this.props.navigation.navigate('Source', {
+                          url: item.source_url,
+                        })
+                      }>
+                      <View style={componentStyles.readMoreViewStyle}>
+                        <Text style={componentStyles.readMoreButtonStyle}>
+                          Read More
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
                   </View>
                   <View style={componentStyles.bottomBarStyle}>
-                      <Icon
-                        name={'random'}
-                        size={21}
-                        color={Colors.iconColor}
-                        style={componentStyles.bottomBarIconStyle}
-                      />
-                      <Icon
-                        name={'comment'}
-                        size={21}
-                        color={Colors.iconColor}
-                        style={componentStyles.bottomBarIconStyle}
-                      />
-                      <TouchableOpacity onPress={() => this.props.navigation.navigate('Suggestions', {url: item.source_url})}>
-                        <Text style={componentStyles.moreStoriesStyle}>
-                            More Stories
-                        </Text>
-                      </TouchableOpacity>
-                      <Icon
-                        name={'arrow-up'}
-                        size={21}
-                        color={Colors.iconColor}
-                        style={componentStyles.bottomBarIconStyle}
-                      />
-                      <Icon
-                        name={'share-alt'}
-                        size={21}
-                        color={Colors.iconColor}
-                        style={componentStyles.bottomBarIconStyle}
-                      />
+                    <Icon
+                      name={'random'}
+                      size={21}
+                      color={Colors.iconColor}
+                      style={componentStyles.bottomBarIconStyle}
+                    />
+                    <Icon
+                      name={'comment'}
+                      size={21}
+                      color={Colors.iconColor}
+                      style={componentStyles.bottomBarIconStyle}
+                    />
+                    <TouchableOpacity
+                      onPress={() =>
+                        this.props.navigation.navigate('Suggestions', {
+                          url: item.source_url,
+                        })
+                      }>
+                      <Text style={componentStyles.moreStoriesStyle}>
+                        More Stories
+                      </Text>
+                    </TouchableOpacity>
+                    <Icon
+                      name={'arrow-up'}
+                      size={21}
+                      color={Colors.iconColor}
+                      style={componentStyles.bottomBarIconStyle}
+                    />
+                    <Icon
+                      name={'share-alt'}
+                      size={21}
+                      color={Colors.iconColor}
+                      style={componentStyles.bottomBarIconStyle}
+                    />
                   </View>
                 </View>
-                </SafeAreaView>
-              );
-            })}
-        </Swiper>
-      );
-    }
+              </SafeAreaView>
+            );
+          })}
+      </Swiper>
+    );
   }
+}
 
 const Stack = createStackNavigator();
 
 function ArticleDetailsScreen({route, navigation}) {
   return (
     <NavigationContainer independent={true}>
-      <Stack.Navigator initialRouteName="ArticleDetailsView" >
-        <Stack.Screen name="ArticleDetailsView"
+      <Stack.Navigator initialRouteName="ArticleDetailsView">
+        <Stack.Screen
+          name="ArticleDetailsView"
           component={ArticleDetailsView}
           options={{headerShown: false}}
           initialParams={{
@@ -306,7 +332,11 @@ function ArticleDetailsScreen({route, navigation}) {
           }}
         />
         <Stack.Screen name="Source" component={SourceScreen} />
-        <Stack.Screen name="Suggestions" component={SuggestionsScreen} options={{headerShown: false}} />
+        <Stack.Screen
+          name="Suggestions"
+          component={SuggestionsScreen}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
