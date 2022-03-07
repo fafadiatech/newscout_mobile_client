68ace4404306fb79ecd544efba3b287182a0e1b5
Arch-020 <archana.gawari@fafadiatech.com>
2022-03-07 12:26:35 +0530

    searched article can navigate to article details page

--- src/NewScout/screens/SearchScreen.js
+++ src/NewScout/screens/SearchScreen.js
@@ -1,70 +1,121 @@
-import * as React from 'react';
-import {
-  Text,
-  View,
-  FlatList,
-  ScrollView
-} from 'react-native';
+import { Text, View, TextInput, ScrollView, FlatList, } from 'react-native'
+import React from 'react'
 
-import styles from '../styles/Base';
-import IconListViewItem from '../components/IconListViewItem';
-import Search from '../components/Search';
+import { SearchBar } from 'react-native-elements';
 
+import LeftImageAlignedItem from '../components/LeftImageAlignedItem';
+import RightImageAlignedItem from '../components/RightImageAlignedItem';
 
-class SearchScreen extends React.Component {
+export class SearchScreen extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {dataSource: {}};
+        super(props);
+        this.state = {
+            searchData: [],
+            // noData: false,
+        }
     }
-  
-    componentDidMount() {
-      let options = [
-        {
-          id: 0,
-          icon: 'users',
-          caption: 'My Profile',
-          supporting: 'Update Profile | Change Passwords',
-        },
-        {
-          id: 1,
-          icon: 'file',
-          caption: 'My Records',
-          supporting: 'Update Records | Delete Records',
-        },
-      ];
-  
-      this.setState({
-        options: options,
-      });
-    }
-    render() {
-      return (
-        <View style={styles.flexible}>
-          <ScrollView
-              showsVerticalScrollIndicator={false}
-              style={styles.flexible}
-          >
-          <Search/>
-            <Text
-              style={styles.screenHeading}>
-              Settings
-            </Text>
-            <FlatList
-              style={styles.flexible}
-              data={this.state.options}
-              renderItem={({item}) => (
-                <IconListViewItem
-                  icon={item.icon}
-                  caption={item.caption}
-                  supporting={item.supporting}
-                />
-              )}
-              keyExtractor={(item, index) => index.toString()}
-            />
-          </ScrollView>
-        </View>
-      );
-    }
-  }
+    search(key) {
+        console.warn(key);
+        fetch("http://www.newscout.in/api/v1/article/search/?domain=newscout&q=" + key)
 
-export default SearchScreen;
+            .then((data) => {
+                data.json()
+                    .then((resp) => {
+                        console.warn("response", resp)
+                        this.setState({
+                            searchData: resp.body.results,
+                            // noData: false,
+                        })
+                        console.warn("resp", this.state.searchData);
+
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                        // this.setState({
+                        //     noData:true,
+                        //     searchData: null,
+                        // });
+                    });
+            })
+
+    }
+
+    render() {
+        return (
+            <View>
+                <View style={{ margin: 10 }}>
+                    <SearchBar
+                        platform="android"
+                        placeholder="Search Here..."
+                        onChangeText={(text) => this.search(text)}
+                    />
+                </View>
+
+                <ScrollView >
+                    {/* {
+                 this.state.searchData && 
+                         this.state.searchData.map((item,index) => {
+                             return (
+                         <View> </View>  )} )}  */}
+
+                    <FlatList
+
+                        data={this.state.searchData}
+
+                        renderItem={({ item, index }) => {
+                            if (index % 2 == 0) {
+                                return (
+                                    <LeftImageAlignedItem
+                                        coverImage={item.cover_image}
+                                        title={item.title}
+                                        source={item.source}
+                                        ts={item.published_on}
+                                        onPressHandler={() => {
+                                            this.props.navigation.push('Article Details', {
+                                                articleID: item.id,
+                                                articleSlug: item.slug,
+                                            });
+                                            //console.log(this.props)
+                                        }}
+                                    />
+                                );
+                            } else {
+                                return (
+                                    <RightImageAlignedItem
+                                        coverImage={item.cover_image}
+                                        title={item.title}
+                                        source={item.source}
+                                        ts={item.published_on}
+                                        onPressHandler={() => {
+                                            this.props.navigation.push('Article Details', {
+                                                articleID: item.id,
+                                                articleSlug: item.slug,
+                                            });
+                                            // console.log(this.props)
+                                        }}
+                                    />
+                                );
+                            }
+                        }}
+                        keyExtractor={(item, index) => index.toString()}
+                    />
+
+                    {/* {
+                        //this.state.searchData.length == 0 &&
+                         this.state.noData &&
+                        (
+                            <View style={{margin: 10, alignSelf: 'center'}}>
+                                <Text>No Data Found</Text>
+                            </View>
+                        )
+                    } */}
+                </ScrollView>
+
+            </View>
+        )
+
+    }
+}
+
+
+export default SearchScreen
