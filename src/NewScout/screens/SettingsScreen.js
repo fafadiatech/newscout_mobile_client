ee6050c994b711b10e55d588a0c60068b876ca8a
Arch-020 <archana.gawari@fafadiatech.com>
2022-03-15 18:26:02 +0530

    changed settings screen

---
+++ src/NewScout/screens/SettingsScreen.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+  FlatList,
+  ScrollView
+} from 'react-native';
+
+import styles from '../styles/Base';
+import IconListViewItem from '../components/IconListViewItem';
+
+
+class SettingsScreen extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {dataSource: {}};
+    }
+  
+    componentDidMount() {
+      let options = [
+        {
+          id: 0,
+          icon: 'users',
+          caption: 'My Profile',
+          supporting: 'Update Profile | Change Passwords',
+        },
+        {
+          id: 1,
+          icon: 'file',
+          caption: 'My Records',
+          supporting: 'Update Records | Delete Records',
+        },
+      ];
+  
+      this.setState({
+        options: options,
+      });
+    }
+    render() {
+      return (
+        <View style={styles.flexible}>
+           <ScrollView
+              showsVerticalScrollIndicator={false}
+              style={styles.flexible}
+          > 
+            <Text
+              style={styles.screenHeading}>
+              Settings
+            </Text>
+            <FlatList
+              style={styles.flexible}
+              data={this.state.options}
+              renderItem={({item}) => (
+                <IconListViewItem
+                  icon={item.icon}
+                  caption={item.caption}
+                  supporting={item.supporting}
+                />
+              )}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </ScrollView>
+        </View>
+      );
+    }
+  }
+
+export default SettingsScreen;
